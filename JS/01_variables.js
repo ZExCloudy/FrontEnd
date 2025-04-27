9eaded3366c2dada2dd2c875bb60a781870bf80e
Pratham <prathamvora987@gmail.com>
2025-04-27 19:21:45 +0530

    variables

---
+++ JS/01_variables.js
@@ -0,0 +1,10 @@
+const id = 123 //Can't update value
+let emailId = "pratham@gmail.com" //value can be changed
+var pasword = "*****" // Prefer not to use because of scope issue 
+myName = "Pratham" // It is possible to declare without providing variable but never use it
+let state
+
+console.log(id)
+
+console.table([id, emailId, pasword, myName, state])
+
