1eecd2ae5cebdb9c78e2fd8343dddd25db31de15
Pratham <prathamvora987@gmail.com>
2025-04-27 19:21:57 +0530

    datatypes

---
+++ JS/02_datatypes.js
@@ -0,0 +1,11 @@
+let name = "Pratham";
+let age = 18;
+let isLearning = true;
+let nullable = null;
+console.table([
+  typeof name,
+  typeof age,
+  typeof isLearning,
+  typeof nullable,
+  typeof undefined,
+]);
