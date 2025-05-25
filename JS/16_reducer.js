4e11b47fe1f364e4082b7472dfd05a9043ed492d
Pratham <prathamvora987@gmail.com>
2025-05-25 20:57:45 +0530

    reducer function

---
+++ JS/16_reducer.js
@@ -0,0 +1,25 @@
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// let sum = array.reduce((acc, currvalue) => {
+//   console.log("Accumulator: ", acc, "Current Value: ", currvalue);
+//   return acc + currvalue;
+// }, 1);
+let sum2 = array.reduce(
+  (acc, currvalue) =>
+    // console.log("Accumulator: ", acc, "Current Value: ", currvalue);
+    acc + currvalue,
+  1
+);
+//console.log("Sum of array elements: ", sum2);
+
+let courses = [
+  { name: "JavaScript", price: 100 },
+  { name: "Python", price: 200 },
+  { name: "Java", price: 300 },
+  { name: "C++", price: 400 },
+];
+
+let totalPrice = courses.reduce((acc, item) => item.price + acc, 0);
+
+console.log("Total Price of Courses: ", totalPrice);
+
